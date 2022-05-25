e04869e2af6ab4cc027694d2543b8d65d21c61f3
mariaedk <mariaedkru@gmail.com>
2022-05-25 00:04:03 -0300

    ajustes footer

--- front-end/projeto-dengue/src/app/components/pessoa/form-pessoa/form-pessoa.component.ts
+++ front-end/projeto-dengue/src/app/components/pessoa/form-pessoa/form-pessoa.component.ts
@@ -3,6 +3,20 @@ import { ViaCep } from './../../../models/via-cep/via-cep';
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 
+class FormContato {
+  nome: string = '';
+  cpf: string = '';
+  dataNasc: string = '';
+  sexo: string = '';
+  situacao: string = '';
+  endereco: ViaCep = new ViaCep({});
+
+  constructor(object: Partial<FormContato>)
+  {
+    Object.assign(this, object);
+  }
+}
+
 @Component({
   selector: 'app-form-pessoa',
   templateUrl: './form-pessoa.component.html',
@@ -11,7 +25,7 @@ import { Subject } from 'rxjs';
 export class FormPessoaComponent implements OnInit
 {
 
-  formContato: ViaCep = {};
+  formContato = new FormContato({});
   showForm = new Subject<boolean>();
   cepInput: string = '';
 
@@ -33,7 +47,7 @@ export class FormPessoaComponent implements OnInit
       cepResponse.subscribe(
         (cepModel) =>
         {
-          this.formContato = cepModel;
+          this.formContato.endereco = cepModel;
           this.showForm.next(true);
         }
       )
@@ -42,13 +56,46 @@ export class FormPessoaComponent implements OnInit
 
   enviarFormPessoa()
   {
-    // TODO:
-  }
+    console.log(`Nome: ${this.formContato.nome}`);
+    console.log(`CPF: ${this.formContato.cpf}`);
+    console.log(`Data de nascimento: ${this.formContato.dataNasc}`);
+    console.log(`Sexo: ${this.formContato.sexo}`);
+    console.log(`Situação: ${this.formContato.situacao}`);
+    console.log(`Rua: ${this.formContato.endereco.logradouro}`);
+    console.log(`Número: ${this.formContato.endereco.numero}`);
+    console.log(`Bairro: ${this.formContato.endereco.bairro}`);
+    console.log(`Cidade: ${this.formContato.endereco.localidade}`);
+    console.log(`UF: ${this.formContato.endereco.uf}`);
+
+
+    let dataAtual = new Date();
+    let dataNascimento = this.formContato.dataNasc;
+    let anoAtual = dataAtual.getFullYear();
+    let mesAtual = dataAtual.getMonth() + 1;
+    let diaAtual = dataAtual.getDate();
+
+    let dataNascimentoList = dataNascimento.split('-');
+
+    let anoNasc = dataNascimentoList[0];
+    let mesNasc = dataNascimentoList[1];
+    let diaNasc = dataNascimentoList[2];
+    let anoNascInt = parseInt(anoNasc);
+    let mesNascInt = parseInt(mesNasc);
+    let diaNascInt = parseInt(diaNasc);
+    let idade = anoAtual - anoNascInt;
+
+    if (mesAtual < mesNascInt) {
+        idade--;
+    } else if (mesAtual == mesNascInt) {
+        if (diaAtual < diaNascInt) {
+            idade--;
+        };
+    };
+   };
 
   resetForm()
   {
-    // TODO: Quando criar o modelo FormPessoa, criar um novo Objeto para resetar o formulário
+    this.formContato = new FormContato({});
   }
-
 
 }
